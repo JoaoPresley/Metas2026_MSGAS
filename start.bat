da05c1f1e3eefbc854523a2186b8091a5f03d7b9
JoaoPresley <9.7265456e+07+JoaoPresley@users.noreply.github.com>
2026-06-23 21:44:16 +0000

    FEAT: Adicionada criação do ambiente virtual

--- start.bat
+++ start.bat
@@ -1,6 +1,17 @@
 @echo off
+set VENV_DIR=venv
+
+if not exist %VENV_DIR% (
+    echo Criando ambiente virtual...
+    python -m venv %VENV_DIR%
+)
+
+echo Ativando ambiente virtual...
+call %VENV_DIR%\Scripts\activate
+
 echo Verificando dependencias...
 pip install -r requirements.txt
+
 echo Iniciando Sistema de Metas MSGAS...
 streamlit run src/app.py
 pause
