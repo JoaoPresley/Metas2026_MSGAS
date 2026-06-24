7caeff516cde456dbdfb26dd52f0427db227c759
JoaoPresley <jvalencarmg@gmail.com>
2026-06-24 15:14:58 -0400

    FIX: inserido ultima modificação à chamada da função

--- src/pages/3_Acompanhamento_Pessoal.py
+++ src/pages/3_Acompanhamento_Pessoal.py
@@ -31,7 +31,9 @@ else:
 
     if compilar:
         try:
-            df = get_compiled_data(file_path)
+            modified_time = os.path.getmtime(file_path)
+
+            df = get_compiled_data(file_path, modified_time)
             st.session_state.compiled_df = df
             st.success("Dados compilados!")
         except Exception as e:
